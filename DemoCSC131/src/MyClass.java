598279fe9faca98782320e3d12ac1d65c8642431
Ong Thao <hellopanmoo@gmail.com>
2019-03-30 11:53:25 -0700

    Transferring files

---
+++ DemoCSC131/src/MyClass.java
@@ -0,0 +1,12 @@
+
+public class MyClass {
+
+<<<<<<< HEAD
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+		System.out.print("Hey");
+	}
+
+=======
+>>>>>>> branch 'master' of https://gitlab.ecs.csus.edu/thaoo/csc131-project.git
+}
---
+++ DemoCSC131/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author canti
+ *
+ */
+module DemoCSC131 {
+}
