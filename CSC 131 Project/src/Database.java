6d8d155d2b66ca51bda6b1d3a6d0216470cc6079
Donnydon <47240696+Donnydon@users.noreply.github.com>
2019-04-14 23:55:07 -0700

    Updated writeToFile().

--- CSC 131 Project/src/Database.java
+++ CSC 131 Project/src/Database.java
@@ -1,7 +1,14 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
 //purpose of this class is to use Person objects to add people onto the database using linked lists or arraylist
 public class Database {
 	private Node front;
-	private String FILENAME;
+	//private String FILENAME = "include/database.txt";
+	//private File text = new File(FILENAME);
 	
 	//constructor method
 	public Database(Node n) {
@@ -44,6 +51,18 @@ public class Database {
 	}
 	
 	private void writeToFile(Node n) {
-		
+		if(n.getP()== null) {
+            System.out.print("This node is empty");
+        }
+        else {
+        	BufferedWriter writer = new BufferedWriter(new FileWriter(new File(/*n.getUsername()*/ "user1" + ".txt")));
+            writer.write(PtoS(n.getP()));
+            writer.close();
+        }
+	}
+	
+	private String PtoS(Person p)
+	{
+		return "";	//cannot write until we can get user info like getUsername();
 	}
 }
